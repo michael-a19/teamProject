95432cf7661d3aea917440efc754c1173a00ba21
Jack Crosthwaite <jackcrosthwaite@sky.com>
2021-05-03 13:31:29 +0100

    Added global banner and nav

--- login/forum/header.php
+++ login/forum/header.php
@@ -11,87 +11,10 @@
 <!-- Begin body -->
 <body id="forumBody">
 
-
-<nav class="navbar"> 
-            <ul class="navbar-nav">
-                <li class="menu-icon" id="menu-button">
-                    <span class="nav-link">
-                        <img class="burger-img" src="../images/icons/burger.svg">
-                        <span class="link-text">A P P - N A M E </span>
-                    </span>
-                </li>
-                <li class="nav-item">
-                    <a href="../index.php" class="nav-link">
-                        <img class="nav-img" src="../images/icons/chevronRight.svg">
-                        <span class="link-text">Home</span>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="planner.php" class="nav-link">
-                        <img class="nav-img" src="../images/icons/chevronRight.svg">
-                        <span class="link-text">Planner</span>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="forum.php" class="nav-link">
-                        <img class ="nav-img" src="../images/icons/chevronRight.svg">
-                        <span class="link-text">Forum</span>
-                    </a>
-                </li>
-
-				<li class="nav-item">
-                    <a href="../chat/index.php" class="nav-link">
-                        <img class ="nav-img" src="../images/icons/chevronRight.svg">
-                        <span class="link-text">Chat</span>
-                    </a>
-                </li>
-
-				<li class="nav-item">
-                    <a href="../login.php" class="nav-link">
-                        <img class ="nav-img" src="../images/icons/chevronRight.svg">
-                        <span class="link-text">Sign in</span>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <img class ="nav-img" src="../images/icons/cog.svg">
-                        <span class="link-text">Settings</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <div id="banner">
-
-        </div>
-
-		<script>
-        let menuButton = document.getElementById('menu-button');
-        menuButton.addEventListener('click', toggleMenu);
-
-        function toggleMenu(){
-            // console.log("works here");
-            // let menu = document.getElementsByClassName('navbar'); 
-
-            // if(menu[0].style.width === '5rem'){
-            //     menu[0].style.width = '16rem';
-            // }else {
-            //     menu[0].style.width = '5rem';
-            // }
-
-            // console.log("fired");
-            
-            
-        }
-    </script>
-
-
-<?php 
-require_once('functions.php');
+<?php
+include("../includes/pagefunctions.inc.php");
+echo createNav();
 echo createBanner();
-
 ?>
 
 <script>
@@ -129,23 +52,5 @@ echo createBanner();
 		<a class="item" href="create_topic.php">Create a topic</a>
 		<a class="item" href="create_cat.php">Create a subject</a>
 
-		<!-- Right hand side login/create account bar -->
-		<div id="infoBar">
-		
-        <!-- <?php
-		error_reporting(E_ALL & ~E_NOTICE);
-		// if user is signed in
-		if($_SESSION['user_id'])
-		{
-			// display message including users user_name. Include link to signout
-			echo 'Welcome <b>' . htmlentities($_SESSION['user_forename']) . '</b>. <a class="item" href="signout.php">Sign out</a>';
-		}
-		else
-		{
-			// if user is not signed in then display signin link or create account
-			echo '<a class="item" href="../login.php">Sign in</a> or <a class="item" href="../register.php">Create an account</a>';
-		}
-		?> -->
-		</div>
 	</div>
 		<div id="mainContent">
